db00cf238bec1318e5f7ab6c1970dec9180b5d5a
saurmo <santiago@creativosdigitales.co>
2021-03-05 07:22:51 -0500

    - cambios css

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <center>Tienda el olimpo</center>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,26 @@
 <template>
-  <div>Hola Mundo</div>
+  <div>
+    Hola Mundo
+
+    <v-btn depressed>
+      Normal
+    </v-btn>
+    <v-btn depressed color="primary">
+      Primary
+    </v-btn>
+    <v-btn depressed color="error">
+      Error
+    </v-btn>
+    <v-btn depressed disabled>
+      Disabled
+    </v-btn>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  layout: "blank",
+};
 </script>
 
 <style></style>
