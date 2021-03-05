89cdbd26c34ddb7cfbcf280b694180eb4e37da20
saurmo <santiago@creativosdigitales.co>
2021-03-05 07:34:27 -0500

    - Creación de proyecto tienda-olimpo

--- tienda-olimpo/pages/index.vue
+++ tienda-olimpo/pages/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <h1>Productos</h1>
+  <div>
+    INICIO
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
-
-<style></style>
--- tienda-olimpo/pages/products.vue
+++ tienda-olimpo/pages/products.vue
@@ -1,5 +1,7 @@
 <template>
-  <Nuxt />
+  <div>
+    <h1>Productos</h1>
+  </div>
 </template>
 
 <script>
