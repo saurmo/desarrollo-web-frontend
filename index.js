ad985b51445193402e669cb8dd2e55d2315ca534
saurmo0108@gmail.com <saurmo0108@gmail.com>
2022-03-09 19:02:17 -0500

    - implementación bootstrap - validación de campos desde js

--- index.js
+++ index.js
@@ -9,20 +9,39 @@ function obtenerValoresFormulario() {
     let clave = document.getElementById("txtClave").value
     let direccion = document.getElementById("txtDireccion").value
 
+    if (correo=="") {
+        // alert("El correo es obligatorio.")
+        document.getElementById("lblAlerta").style.display = ""
+        document.getElementById("lblAlerta").innerText="El correo es obligatorio."
+        return
+    }
+
     // Formas de crear un objeto
     // Inicializar un objeto con propiedades
-    let usuario = { celular, "clave":clave }
+    let usuario = { celular, "clave": clave }
 
     //Agregar propiedades a un JSON
     usuario.correo = correo
     usuario.direccion = direccion
 
     //Agregar propiedades con Bracket notation ["key"]
-    usuario["nombre"] = nombre 
+    usuario["nombre"] = nombre
 
     // Forma resumida de crear el objeto 
     // let usuario = { correo, nombre, celular, clave, direccion}
 
+    document.getElementById("txtCorreo").value = ""
+    document.getElementById("txtNombre").value = ""
+    document.getElementById("txtCelular").value = ""
+    document.getElementById("txtClave").value = ""
+    document.getElementById("txtDireccion").value = ""
     console.log(usuario);
+
 }
 
+
+function cargarPagina() {
+    document.getElementById("lblAlerta").style.display = "none"
+}
+
+cargarPagina()
