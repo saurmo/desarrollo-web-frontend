78c71aa8faf3f96bebcdb5dacc4ff363e4e59354
saurmo0108@gmail.com <saurmo0108@gmail.com>
2022-09-01 19:39:26 -0500

    layouts

---
+++ layouts/blank.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <center>
+      <h1>Frutas y verduras la esquina fresca.</h1>
+      <h3>Siempre orgánico y fresco.</h3>
+    </center>
+
+    <Nuxt />
+  </div>
+</template>
+<script>
+export default {};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
-    <h1>Frutas y verduras la esquina fresca.</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut perspiciatis
+      ab cumque enim nihil odit labore? Ipsam, similique ex maiores accusantium
+      praesentium dolorum tenetur numquam veniam cupiditate, necessitatibus
+      impedit quos.
+    </p>
   </div>
 </template>
 
 <script>
 export default {
-
+  layout:"blank"
 };
 </script>
