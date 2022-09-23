6ade460ebd571b178476cae047ea84e8f03bfbe4
saurmo0108@gmail.com <saurmo0108@gmail.com>
2022-09-22 19:32:48 -0500

    Crear producto

--- pages/productos/index.vue
+++ pages/productos/index.vue
@@ -3,13 +3,24 @@
     <b-container>
       <h1>Gestión de productos</h1>
       <b-form action="javascript:void(0)" @submit="createProduct()">
+        <!-- ID -->
+        <b-form-group id="id_group" label="Código:" label-for="id">
+          <b-form-input
+            placeholder="Ingrese el código del producto"
+            type="text"
+            id="id"
+            v-model="product.id"
+            required
+          ></b-form-input>
+        </b-form-group>
+
         <!-- Nombre -->
         <b-form-group id="name_group" label="Nombre:" label-for="name">
           <b-form-input
-            required
             placeholder="Ingrese el nombre del producto"
             type="text"
             id="name"
+            required
             v-model="product.name"
           ></b-form-input>
         </b-form-group>
@@ -18,19 +29,19 @@
         <b-form-group id="type_group" label="Tipo:" label-for="type">
           <b-form-select
             id="tipo"
+            required
             v-model="product.type"
             :options="product_types"
-            required
           ></b-form-select>
         </b-form-group>
 
         <!-- Precio -->
         <b-form-group id="name_group" label="Precio:" label-for="price">
           <b-form-input
-            required
             placeholder="Ingrese el precio del producto"
             type="number"
             id="price"
+            required
             v-model="product.price"
           ></b-form-input>
         </b-form-group>
@@ -84,20 +95,33 @@ export default {
   // El state (data) se une a los inputs del html por medio del v-model
   data() {
     return {
-      message: "Hello Vue!",
       product: {},
-      nombre: "",
       product_types: ["Verdura", "Fruta"],
     };
   },
   // Metodos: Metodos a utilizar
   methods: {
-    changeMessage() {
-      this.message = "Buenas tardes, dllo web";
+    validateState(name) {
+      const { $dirty, $error } = this.$v.form[name];
+      return $dirty ? !$error : null;
     },
-
     createProduct() {
-      console.log(this.product);
+  
+      const url = "http://localhost:3001/productos";
+      // this.$axios (Lib de axios instalada como complemento)
+
+      // Creación del httpRequest con el method post
+      this.$axios
+        .post(url, this.product)
+        .then((response) => {
+          // THEN solución de la promesa cuando resuelve (resolve)
+          alert(response.data.message);
+        })
+        .catch((error) => {
+          // CATCH solución de la promeda cuando rechaza (reject)
+          console.error(error);
+          alert("Ha ocurrido un error");
+        });
     },
   },
 };
