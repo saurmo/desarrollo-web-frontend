f05693ce363b3a446680d106bbb2661a20dd98f8
saurmo0108@gmail.com <saurmo0108@gmail.com>
2023-02-25 16:30:11 -0500

    Se crea layout en blanco

---
+++ layouts/blank.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'BlankLayout',
+  data() {
+    return {
+
+    }
+  },
+}
+</script>
