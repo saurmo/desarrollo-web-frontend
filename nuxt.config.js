b772fe9a41776c33aba238dbf48c5730415b35ae
jahlon <jahlon@gmail.com>
2023-03-02 18:29:45 -0500

    Layout en blanco

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,7 +50,6 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
       themes: {
         dark: {
           primary: colors.blue.darken2,
---
+++ pages/register.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-container>
+    <h1>Hola Mundo</h1>
+  </v-container>
+</template>
+<script lang="ts">
+export default {
+  layout: 'blank',
+}
+</script>
