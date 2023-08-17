63b8e418b474a06b620d46e3d51bbb6c523ffd78
jesus hincapie <jahlon@gmail.com>
2023-08-17 18:49:19 -0500

    feat: get data form desde js

--- app-tasks-bases/index.js
+++ app-tasks-bases/index.js
@@ -1,4 +1,21 @@
 
 const getDataForm = () => {
-    
+    const txtName = document.getElementById("txtName").value
+    const txtEmail = document.getElementById("txtEmail").value
+    const txtDescription = document.getElementById("txtTask").value
+    const acceptCondition = document.getElementById("acceptConditions").checked
+    const typeA = document.getElementById("typeA").checked
+    const typeB = document.getElementById("typeB").checked
+
+    // JSON { clave:valor }
+    const data = {
+        name: txtName,
+        email: txtEmail,
+        description:txtDescription,
+        acceptCondition:acceptCondition,
+        typeA,
+        typeB
+    }
+
+    console.log(data);
 }
